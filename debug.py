1cf4f4bf79f1dcc519c952bbb42468b15f790693
wuxianao <100472806+wuxianao@users.noreply.github.com>
2023-04-09 14:35:34 +0800

    change some content

--- debug.py
+++ debug.py
@@ -1,10 +1,7 @@
-def power(x, n): # 乘方
-    s = 1
-    while n > 0:
-        n = n - 1
-        s = s * x
-    return s
-
-#测试
-print(power(3, 2))
-print(power(2, 3))
+def fact(x: int):
+    if x == 0:
+        return 1
+    else:
+        return x * fact(x-1)
+    
+print(fact(3))
